fd5255fe7468d6814c562299e92e284a5abcc925
ayush <srihari.ayush@gmail.com>
2017-09-06 16:38:18 +0530

    added omments

--- ex3.py
+++ ex3.py
@@ -1,24 +1,25 @@
+# first line
 print("I will now count my chickens:")
-
+#printed no of hens and roosters
 print("Hens", 25 + 30 / 6)
 print("Roosters", 100 - 25 * 3 % 4)
-
+#printed a string
 print("Now I will count the eggs:")
-
+#operation to count no of eggs
 print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
-
+#printed a string
 print("Is it true that 3 + 2 < 5 - 7?")
-
+#operation 
 print(3 + 2 < 5 - 7)
-
+#2 arithmetic questions and answers
 print("what is 3 + 2?", 3 + 2)
 print("what is 5 - 7?", 5 - 7)
-
+#printed a string
 print("Oh, that's why it's False.")
-
+#print string
 print("How about some more.")
-
+#printed a string and its numerical operation
 print("Is it greater?", 5 > -2)
-
+#printed 2 strings and its numerical operations
 print("Is it greater or equal?", 5 >= -2)
 print("Is it less or equal?", 5 <=- 2)
