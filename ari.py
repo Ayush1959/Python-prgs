2f37627dc25b5e35d033986e24a0cc192fbe73eb
ayush <srihari.ayush@gmail.com>
2017-09-11 11:59:19 +0530

    Added several changes

--- ari.py
+++ ari.py
@@ -1,24 +1,35 @@
-def ari (x):
-    f=open(x)
-    no_words = 0
-    charecters = 0
-    table={ 1: 'Kintergarden', 2: 'First grade', 3: 'second grade', 4: 'third grade', 5: 'fourth grade', 6: 'fifth grade', 7: 'sith grade', 8: 'seventh grade', 9: 'eigth grade', 10: 'nineth grade', 11: 'tenth grade', 12: 'eleventh grade', 13: 'tewelveth grade', 14: 'college'}
+def ari (filename):
+    f=open(filename)
+    nwords = 0
+    nchars = 0
+    reading_level={ 1: 'Kintergarden',
+                    2: 'First grade',
+                    3: 'second grade',
+                    4: 'third grade',
+                    5: 'fourth grade',
+                    6: 'fifth grade',
+                    7: 'sith grade',
+                    8: 'seventh grade',
+                    9: 'eigth grade',
+                    10: 'nineth grade',
+                    11: 'tenth grade',
+                    12: 'eleventh grade',
+                    13: 'tewelveth grade',
+                    14: 'college'}
     content=f.readlines()
+    f.close()
     l=len(content)
     for i in content:
-        words = i.split()     
-        no_words = no_words + len(words)
-        charecters = charecters + len(i)
-    a= charecters / no_words
-    b= no_words / l
-    c= 4.71 * a + 0.5* b -21.43
-    d= round (c)
-    if c > d:
-        e=d + 1
-    else:
-        e=d
+        words = i.split(" ")     
+        nwords = nwords + len(words)
+        nchars = nchars + len(i)
+    a= nchars / nwords
+    b= nwords / l
+    ari= 4.71 * (nchars/nwords) + 0.5 * (nwords/l) - 21.43
+    import math
+    e=math.ceil(ari)
     if e > 14:
         print("Automatic readability index gives a score above 14,so the book is readable by college students")
     else:
-        t=table[e]
+        t=reading_level[e]
         print("Automatic readability index gives a score of {},so the book is readable by {} students".format(e, t ))
