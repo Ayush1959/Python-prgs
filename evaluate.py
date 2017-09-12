d180be51d885320799dfb0d0044336b0655c3ccf
ayush <srihari.ayush@gmail.com>
2017-09-12 18:31:37 +0530

    reduced the function length using 'import oerator' function, added error printing if expression is wrong

--- evaluate.py
+++ evaluate.py
@@ -1,27 +1,25 @@
-def evaluate (n):
+def evaluate (post_fix_string):
     l=[]
-    for i in n:
-        if i.isdigit() == True:
-            l.append(i)
-        else:
-            t=int(l.pop())
-            s=int(l.pop())
-            if i == '+':
-                z=t+s
-                l.append(z)
-            elif i == '-':
-                z=t-s
-                l.append(z)
-            elif i == '*':
-                z=t*s
-                l.append(z)
-            elif i == '/':
-                z=t/s
-                l.append(z)
-            elif i == '%':
-                z=t%s
-                l.append(z)
+    for i in post_fix_string:
+        try:
+            if i.isdigit():
+                l.append(i)
             else:
-                z="error"
+                t=int(l.pop())
+                s=int(l.pop())
+                import operator
+                ops = { '+' : operator.add,
+                    '-' : operator.sub,
+                    '*' : operator.mul,
+                    '/' : operator.truediv,
+                    '%' : operator.mod,
+                    '^' : operator.xor
+                   }
+                z=ops[i](t,s)
+                l.append(z)
+        except IndexError:
+                    print ("Expression Error")
+    
+    
     return l
 
