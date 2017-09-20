a1f2e1834a6d280d1f0cc2ebaab0f65c1301c1aa
ayush <srihari.ayush@gmail.com>
2017-09-20 17:30:00 +0530

    test for postfix evaluation in all forms

--- test_evaluate.py
+++ test_evaluate.py
@@ -1,8 +1,21 @@
 import evaluate
 
 def test_add():
-    c=evaluate.evaluate("55+")
-    e=[10]
+    c=evaluate.evaluate("545++")
+    e=[14]
     assert c == e
-    
-    
+
+def test_sub():
+    csub = evaluate.evaluate("455-+")
+    esub = [4]
+    assert csub == esub
+
+def test_mul():
+    cmul = evaluate.evaluate("5455-+*")
+    emul = [20]
+    assert cmul == emul
+
+def test_div():
+    cdiv = evaluate.evaluate("45455-+*/")
+    ediv = [0.2]
+    assert cdiv == ediv
